a5058361d5ed6cfb075b5828a39da15fb1823354
Anzlc <107500985+Anzlc@users.noreply.github.com>
2026-06-01 11:40:54 +0200

    The results page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { App } from './app';
 import { Inverter } from './inverter/inverter';
 import { Main } from './main/main';
+import { Results } from './results/results';
 
 export const routes: Routes = [
   { path: 'inverter/:id', component: Inverter },
+  { path: 'results', component: Results },
   { path: '', component: App },
 ];
---
+++ src/app/results/results.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-results',
+  imports: [],
+  templateUrl: './results.html',
+  styleUrl: './results.css',
+})
+export class Results {
+  private http = inject(HttpClient);
+  private router = inject(Router);
+  results: WritableSignal<any> = signal(null);
+  constructor() {
+    this.http.get('http://localhost:5000/get-all-jobs').subscribe({
+      next: (res: any) => {
+        this.results.set(
+          res.results.map((el: any) => ({
+            ...el,
+            data: JSON.parse(el.data),
+          })),
+        );
+      },
+    });
+  }
+  openSpecific(id: string) {
+    this.router.navigate(['/inverter', id]);
+  }
+}
